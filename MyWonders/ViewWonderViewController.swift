83446aff6418939332876b19b8e54df73db8fcee
Mélissa BEUZE <melissa.beuze@gmail.com>
2016-11-26 05:45:05 +0100

    Add VC (story +class) to wonder list element on click

---
+++ MyWonders/ViewWonderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewWonderViewController.swift
+//  MyWonders
+//
+//  Created by Mélissa BEUZE on 26/11/16.
+//  Copyright © 2016 Mélissa BEUZE. All rights reserved.
+//
+
+import UIKit
+
+class ViewWonderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
